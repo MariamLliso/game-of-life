3fc3ceb8d9b1fdc73f607e361d0b7eabfee3af29
Mariam Lliso <contacto.mariamlliso@gmail.com>
2022-04-15 18:35:26 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "lines-between-class-members": [
+    "error",
+    "always",
+    { exceptAfterSingleLine: true },
+  },
+};
